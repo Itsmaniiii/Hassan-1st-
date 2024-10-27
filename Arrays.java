c1aa23880039160f6f711369d1d96bea180dd847
MUHAMMAD HASSAN <125796512+Itsmaniiii@users.noreply.github.com>
2024-10-27 20:13:34 +0500

    Add files via upload
    "Initial commit with Java project files."

---
+++ Arrays.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class Arrays {
+    public static void main(String [] args){
+        int [] arr = {255,287,299,153};
+        System.out.println(Arrays.toString(arr));
+        
+        
+        
+        
+    }
+    
+}
