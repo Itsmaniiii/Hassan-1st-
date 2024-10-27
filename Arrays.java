9218ba7124629f8dd32fdd85e1addc73bc24d2e0
MUHAMMAD HASSAN <125796512+Itsmaniiii@users.noreply.github.com>
2024-10-27 20:29:02 +0500

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class Arrays {
     public static void main(String [] args){
-        int [] arr = {255,287,299,153};
+        int [] arr = {255,287,153};
         System.out.println(Arrays.toString(arr));
         
         
